a70953804e53063500d3c62f3310bf3cdb67ca79
Tiago Pires <tiago.pires@me.com>
2024-01-01 19:22:16 +1000

    Add basic plugins

--- init.lua
+++ init.lua
@@ -26,7 +26,65 @@ vim.opt.rtp:prepend(lazypath)
 
 -- 2 load lazy
 
-local plugins = {}
+local plugins = { 
+  { "catppuccin/nvim", name = "catppuccin", opts = {}, priority = 1000 },
+  { "tpope/vim-fugitive" },
+  {   
+    "stevearc/oil.nvim",
+    opts = {
+      columns = {
+        "icon",
+        -- "size",
+        -- "permissions",
+        -- "mtime",
+      },
+      win_options = {
+        signcolumn = "yes",
+      },
+      delete_to_trash = true,
+    },
+    keys = {
+       { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    },
+  },
+  { 
+     "nvim-telescope/telescope.nvim", branch = '0.1.x', dependencies = {'nvim-lua/plenary.nvim'}, opts = {}, 
+     keys = {
+       { "<c-p>", "<cmd>Telescope find_files<cr>", desc = "Fuzzy find files" },
+     },
+  },
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      -- TODO: use vim.defer_fn to improve startup time (see kickstarter)
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = { 'lua', 'python', 'tsx', 'php', 'markdown', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+        highlight = { enable = true },
+        indent = {enable = true},
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<M-space>',
+          },
+        },
+    }) end,
+    opts = {},
+    build = ':TSUpdate',
+  }
+}
+
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+vim.cmd.colorscheme "catppuccin"
+
+
