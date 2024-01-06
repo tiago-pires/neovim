8f0590bbfd7d2fda00f6308141d18bc2bd738b8f
Tiago Pires <tiago.pires@me.com>
2024-01-07 08:55:45 +1000

    formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,26 +1,23 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local none_ls = require("null-ls") -- project still refers to old null-ls, for now
-    none_ls.setup({
-      sources = {
-        -- lua
-        none_ls.builtins.formatting.stylua, -- install with Mason 
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local none_ls = require("null-ls") -- project still refers to old null-ls, for now
+		none_ls.setup({
+			sources = {
+				-- lua
+				none_ls.builtins.formatting.stylua, -- install with Mason
 
-        -- javascript
-        none_ls.builtins.formatting.prettier,
-        none_ls.builtins.diagnostics.eslint_d,
+				-- javascript
+				none_ls.builtins.formatting.prettier,
+				-- none_ls.builtins.diagnostics.eslint_d,
 
-        -- php
+				-- php
 
-
-        -- python
-        -- none_ls.builtins.formatting.black,
-        -- none_ls.builtins.diagnostics.isort
-
-
-      },
-    })
-    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-  end,
+				-- python
+				-- none_ls.builtins.formatting.black,
+				-- none_ls.builtins.diagnostics.isort
+			},
+		})
+		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+	end,
 }
